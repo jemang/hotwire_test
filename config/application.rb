64f833a7e58aa196398a8ad59c8ffacc08743f90
Muhamad Azman <jempalo94@gmail.com>
2021-11-25 17:00:43 +0800

    update domain

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module HotwireTest
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "hotwire.jemang.my"
   end
 end
